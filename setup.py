71bfdf39116de0c7ff3acf1cd81a881efd865177
Felix Geilert <root92@live.de>
2022-04-09 12:04:40 +0200

    Feat: Fixed case sensitivity

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ __author__      = "Felix Geilert"
 
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, 'readme.md'), encoding='utf-8') as f:
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 
