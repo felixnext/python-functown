8c69d39d27ab82459a98e9c99f126ca2a5433349
Felix Geilert <root92@live.de>
2023-01-28 10:50:33 +0100

    Chore: Added more comprehensive unit-tests

--- tests/serialization/test_json.py
+++ tests/serialization/test_json.py
@@ -4,9 +4,12 @@ Copyright (c) 2023, Felix Geilert
 """
 
 import json
+from typing import Any
 
 from azure.functions import HttpRequest, HttpResponse
+import pytest
 
+from functown.errors import RequestError
 from functown.serialization import JsonResponse, JsonRequest
 
 
@@ -43,27 +46,144 @@ def test_json_response():
     assert json.loads(res.get_body()) == {"hello": "world"}
 
 
-def test_json_request():
+@pytest.mark.parametrize(
+    "plain, enforce, data, mime, dumps, encode, key, error",
+    [
+        # basic (dumped)
+        (
+            True,
+            False,
+            {"foo": "bar"},
+            "application/json",
+            True,
+            False,
+            "foo",
+            False,
+        ),
+        # basic (dumped, encoded)
+        (
+            True,
+            False,
+            {"foo": "bar"},
+            "application/json",
+            True,
+            True,
+            "foo",
+            False,
+        ),
+        # basic (not dumped) - fail
+        (
+            True,
+            False,
+            {"foo": "bar"},
+            "application/json",
+            False,
+            False,
+            "foo",
+            True,
+        ),
+        # invalid mime - fail
+        (
+            True,
+            False,
+            {"foo": "bar"},
+            "text/plain",
+            True,
+            True,
+            "foo",
+            True,
+        ),
+        # not plain
+        (
+            False,
+            True,
+            {"foo": "bar"},
+            "application/json",
+            True,
+            False,
+            "foo",
+            False,
+        ),
+        # not plain, invalid mime - fail
+        (
+            False,
+            True,
+            {"foo": "bar"},
+            "text/plain",
+            True,
+            False,
+            "foo",
+            True,
+        ),
+        # not plain, invalid mime, no enforce
+        (
+            False,
+            False,
+            {"foo": "bar"},
+            "text/plain",
+            True,
+            False,
+            "foo",
+            False,
+        ),
+        # invalid data type
+        (True, False, [1, 2, 3], "application/json", False, False, "foo", True),
+    ],
+    ids=[
+        "basic (dumped)",
+        "basic (dumped, encoded)",
+        "basic (not dumped) - fail",
+        "invalid mime - fail",
+        "not plain",
+        "not plain, invalid mime - fail",
+        "not plain, invalid mime, no enforce",
+        "invalid data type",
+    ],
+)
+def test_json_request(
+    plain: bool,
+    enforce: bool,
+    data: Any,
+    mime: str,
+    dumps: bool,
+    encode: bool,
+    key: str,
+    error: bool,
+):
     """Tests JSON request deserialization."""
 
-    @JsonRequest
+    if plain:
+        dec = JsonRequest
+    else:
+        dec = JsonRequest(enfore_mime=enforce)
+
+    @dec
     def main(req: HttpRequest, body: dict):
-        return body["hello"]
+        return body[key]
 
-    res = main(
-        HttpRequest("GET", "http://localhost", body=json.dumps({"hello": "world"}))
-    )
-    assert res == "world"
+    body_data = data
+    if dumps:
+        body_data = json.dumps(data)
+    if encode:
+        body_data = body_data.encode("utf-8")
 
-    @JsonRequest()
-    def main(req: HttpRequest, body: dict):
-        return body["hello"]
-
-    res = main(
-        HttpRequest(
-            "GET",
-            "http://localhost",
-            body=json.dumps({"hello": "world"}).encode("utf-8"),
+    if error:
+        with pytest.raises(RequestError):
+            res = main(
+                HttpRequest(
+                    "GET",
+                    "http://localhost",
+                    body=body_data,
+                    headers={"Content-Type": mime},
+                )
+            )
+    else:
+        res = main(
+            HttpRequest(
+                "GET",
+                "http://localhost",
+                body=body_data,
+                headers={"Content-Type": mime},
+            )
         )
-    )
-    assert res == "world"
+        assert res == data[key]
