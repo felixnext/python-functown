e70368e8034c0e9bcc2a2eb916267e632cb96fdc
Felix Geilert <root92@live.de>
2023-01-31 10:15:24 +0100

    Feat: updated unit tests for dataframe serialization

--- tests/serialization/test_dataframe.py
+++ tests/serialization/test_dataframe.py
@@ -6,9 +6,17 @@ Copyright (c) 2023, Felix Geilert
 from azure.functions import HttpRequest, HttpResponse
 import pandas
 import pytest
+from io import StringIO, BytesIO
 
 from functown.args import ContentTypes
-from functown.serialization import DataFrameResponse, DataFrameRequest
+from functown.serialization import DataFrameResponse, DataFrameRequest, DataFrameFormat
+
+
+DF_MAP = {
+    DataFrameFormat.CSV: ContentTypes.CSV,
+    DataFrameFormat.JSON: ContentTypes.JSON,
+    DataFrameFormat.PARQUET: ContentTypes.BINARY,
+}
 
 
 @pytest.fixture
@@ -20,22 +28,19 @@ def df_data():
 @pytest.mark.parametrize(
     "ctype, allow_empty, is_empty, error",
     [
-        (ContentTypes.CSV, True, False, None),
-        (ContentTypes.CSV, True, True, None),
-        (ContentTypes.CSV, False, False, None),
-        (ContentTypes.CSV, False, True, ValueError),
-        (ContentTypes.JSON, True, False, None),
-        (ContentTypes.JSON, True, True, None),
-        (ContentTypes.JSON, False, False, None),
-        (ContentTypes.JSON, False, True, ValueError),
-        (ContentTypes.PARQUET, True, False, None),
-        (ContentTypes.PARQUET, True, True, None),
-        (ContentTypes.PARQUET, False, False, None),
-        (ContentTypes.PARQUET, False, True, ValueError),
+        (DataFrameFormat.CSV, False, False, None),
+        (DataFrameFormat.CSV, True, True, None),
+        (DataFrameFormat.CSV, False, True, ValueError),
+        (DataFrameFormat.JSON, False, False, None),
+        (DataFrameFormat.JSON, True, True, None),
+        (DataFrameFormat.JSON, False, True, ValueError),
+        (DataFrameFormat.PARQUET, False, False, None),
+        (DataFrameFormat.PARQUET, True, True, None),
+        (DataFrameFormat.PARQUET, False, True, ValueError),
     ],
 )
 def test_dataframe_response(
-    df_data, ctype: ContentTypes, allow_empty: bool, is_empty: bool, error: Exception
+    df_data, ctype: DataFrameFormat, allow_empty: bool, is_empty: bool, error: Exception
 ):
     # test the response for different
     @DataFrameResponse(format=ctype, allow_empty=allow_empty)
@@ -51,7 +56,7 @@ def test_dataframe_response(
     # test response
     res = main(req=req)
     assert isinstance(res, HttpResponse)
-    assert res.mimetype == ctype.value
+    assert res.mimetype == DF_MAP[ctype.value]
 
     # check empty
     if is_empty:
@@ -59,11 +64,11 @@ def test_dataframe_response(
         return
 
     # decode dataframe based on content type
-    if ctype == ContentTypes.CSV:
-        df = pandas.read_csv(res.get_body().decode("utf-8"))
-    elif ctype == ContentTypes.JSON:
-        df = pandas.read_json(res.get_body().decode("utf-8"))
-    elif ctype == ContentTypes.PARQUET:
-        df = pandas.read_parquet(res.get_body())
+    if ctype == DataFrameFormat.CSV:
+        df = pandas.read_csv(StringIO(res.get_body().decode("utf-8")))
+    elif ctype == DataFrameFormat.JSON:
+        df = pandas.read_json(StringIO(res.get_body().decode("utf-8")))
+    elif ctype == DataFrameFormat.PARQUET:
+        df = pandas.read_parquet(BytesIO(res.get_body()))
     assert df is not None
     assert df.equals(df_data)
